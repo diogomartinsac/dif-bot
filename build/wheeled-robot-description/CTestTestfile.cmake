a89bff92e0c130cd6d9923d85a6f8dff57e7f5d9
diogo <diogomartins.ac@gmail.com>
2019-12-19 16:06:27 -0300

    Initial commit

---
+++ build/wheeled-robot-description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/diogo/wheeled_robot_ws/src/wheeled-robot-description
+# Build directory: /home/diogo/wheeled_robot_ws/build/wheeled-robot-description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
